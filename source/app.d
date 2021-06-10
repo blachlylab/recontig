ccecfa9a24c1d15c4323cdfb61ba6a30529d180b
Charles Gregory <charles.gregory940@topper.wku.edu>
2021-06-10 01:25:16 -0400

    added ability to change filename of ejected output

--- source/app.d
+++ source/app.d
@@ -1,9 +1,9 @@
 import std.stdio;
 import std.getopt;
 import std.conv : to;
+import std.array : join;
 
-import mapping;
-import vcf;
+import recontig;
 
 enum InputFileType
 {
@@ -18,17 +18,21 @@ enum InputFileType
 string build;
 string conversion;
 InputFileType type;
+string ejectedfn;
+string mappingFile;
 
 void main(string[] args)
 {
+	auto clstr = args.join(" ");
 	auto res = getopt(args, 
 			config.required, "build|b","Genome build i.e GRCh37", &build, 
 			config.required, "conversion|c", "Conversion string i.e UCSC2ensembl", &conversion,
-			config.required, "file-type|f", "Type of file to convert i.e vcf", &type
+			config.required, "file-type|f", "Type of file to convert i.e vcf", &type,
+			"ejected-output", "File to write ejected records to (records with unmapped contigs)", &ejectedfn
 		);
 	if (res.helpWanted | (args.length < 2))
 	{
-		defaultGetoptPrinter("recontig: remap contig names for different bioinformatic file types",
+		defaultGetoptPrinter("recontig: remap contig names for different bioinformatics file types",
 				res.options);
 		stderr.writeln();
 		return;
@@ -49,23 +53,52 @@ void main(string[] args)
 		if(c == conversion) convFound = true, convIdx = i;
 	}
 	if(!convFound) throw new Exception("Please use a valid conversion: " ~ CONVERSIONS[buildIdx].to!string);
+	if(ejectedfn == "")
+	{
+		switch(type)
+		{
+			case InputFileType.vcf:
+			case InputFileType.bcf:
+				ejectedfn = "ejected.vcf";
+				break;
+			case InputFileType.gff:
+				ejectedfn = "ejected.txt";
+				break;
+			case InputFileType.bed:
+				ejectedfn = "ejected.bed";
+				break;
+			case InputFileType.bam:
+			case InputFileType.sam:
+				ejectedfn = "ejected.sam";
+				break;
+			default:
+				stderr.writeln("Error: Filetype not supported.");
+				return;
+		}
+	}
+
+	auto mapping = getContigMapping(build, conversion);
 
 	switch(type)
 	{
 		case InputFileType.vcf:
 		case InputFileType.bcf:
-			recontigVCF(args, build, conversion);
+			recontigVcf(args[1], ejectedfn, mapping, clstr);
 			break;
 		case InputFileType.gff:
+			recontigGff(args[1], ejectedfn, mapping, clstr);
+			break;
 		case InputFileType.bed:
+			recontigBed(args[1], ejectedfn, mapping, clstr);
+			break;
 		case InputFileType.bam:
+			recontigBam(args[1], ejectedfn, mapping, clstr);
+			break;
 		case InputFileType.sam:
-			stderr.writeln("Error: Filetype not supported yet.");
+			recontigSam(args[1], ejectedfn, mapping, clstr);
 			return;
 		default:
 			stderr.writeln("Error: Filetype not supported.");
 			return;
 	}
-
-	
 }
