7abb74af933c6963d6554745a9dc3808cea623d7
Charles Gregory <charles.gregory940@topper.wku.edu>
2021-06-10 13:48:23 -0400

    cleaned up cl interface

--- source/app.d
+++ source/app.d
@@ -2,42 +2,88 @@ import std.stdio;
 import std.getopt;
 import std.conv : to;
 import std.array : join;
+import std.format : format;
+import std.traits : EnumMembers;
+import std.algorithm : map;
 
 import dhtslib.bgzf;
 import recontig;
 
 enum InputFileType
 {
+	None,
 	vcf,
+	bcf,
 	bam,
 	sam,
-	bcf,
 	bed,
 	gff
 }
 
 string build;
 string conversion;
-InputFileType type;
+InputFileType type = InputFileType.None;
 string ejectedfn;
 string mappingfn;
+string HELP =  
+"recontig: remap contig names for different bioinformatics file types.
+                
+usage: recontig [-e ejected.txt] [-m mapping.txt | -b build -c conversion] <in.file>
 
-void main(string[] args)
+Input can be any of the following formats: vcf, bcf, bam, sam, bed, gff              
+Input can be compressed with gzip or bgzf and can be accessed remotely via https or s3 (see htslib for details).
+
+use 'recontig build-help' to check availiable builds                                 
+use 'recontig -b build' conversion-help to check availiable conversions for a build
+";
+
+int main(string[] args)
 {
 	auto clstr = args.join(" ");
 	auto res = getopt(args, 
-			config.required, "file-type|f", "Type of file to convert i.e vcf", &type,
+			"file-type|f", "Type of file to convert (vcf, bcf, bam, sam, bed, gff)", &type,
 			"build|b","Genome build i.e GRCh37 for using dpryan79's files", &build, 
 			"conversion|c", "Conversion string i.e UCSC2ensembl for using dpryan79's files", &conversion,
-			"mapping", "If want to use your own remapping file instead of dpryan79's", &mappingfn,
-			"ejected-output", "File to write ejected records to (records with unmapped contigs)", &ejectedfn
+			"mapping|m", "If want to use your own remapping file instead of dpryan79's", &mappingfn,
+			"ejected-output|e", "File to write ejected records to (records with unmapped contigs)", &ejectedfn
 		);
-	if (res.helpWanted | (args.length < 2))
+	
+	if (res.helpWanted || (args.length < 2))
 	{
-		defaultGetoptPrinter("recontig: remap contig names for different bioinformatics file types",
+		defaultGetoptPrinter(HELP,
 				res.options);
 		stderr.writeln();
-		return;
+		return 0;
+	}
+
+	if(args[1] == "build-help"){
+		stderr.writeln("Valid builds: " ~ BUILDS.to!string);
+		return 0;
+	}
+	
+	if(args[1] == "conversion-help"){
+		if(build == ""){
+			stderr.writeln("Error: -b was not set. Need valid build to provide availiable conversions");
+			return 1;
+		}
+		bool buildFound;
+		ulong buildIdx = 0;
+		foreach(i, b;BUILDS){
+			if(b == build) buildFound = true, buildIdx = i;
+		}
+		if(!buildFound){
+			stderr.writeln("Error: Please use a valid build: " ~ BUILDS.to!string);
+			return 1;
+		}
+		stderr.writeln("Valid conversions for " ~ build ~ ": " ~ CONVERSIONS[buildIdx].to!string);
+		return 0;
+	}
+
+	if (type == InputFileType.None)
+	{
+		defaultGetoptPrinter("Error: -f or --file-type is required.\n" ~ HELP,
+				res.options);
+		return 1;
 	}
 	
 	/// if no ejected filename provided we will select a default name
@@ -61,7 +107,7 @@ void main(string[] args)
 				break;
 			default:
 				stderr.writeln("Error: Filetype not supported.");
-				return;
+				return 1;
 		}
 	}
 
@@ -72,7 +118,7 @@ void main(string[] args)
 	else{
 		if(build =="" || conversion == ""){
 			stderr.writeln("Error: if not using a mapping file you must provide a valid build and conversion.");
-			return;
+			return 1;
 		}
 		// validate build
 		bool buildFound;
@@ -82,7 +128,7 @@ void main(string[] args)
 		}
 		if(!buildFound){
 			stderr.writeln("Error: Please use a valid build: " ~ BUILDS.to!string);
-			return;
+			return 1;
 		}
 
 		//validate conversion
@@ -93,7 +139,7 @@ void main(string[] args)
 		}
 		if(!convFound){
 			stderr.writeln("Error: Please use a valid conversion: " ~ CONVERSIONS[buildIdx].to!string);
-			return;
+			return 1;
 		}
 		mappingFile = getDpryan79ContigMappingFile(build, conversion);
 	}
@@ -119,9 +165,10 @@ void main(string[] args)
 			break;
 		case InputFileType.sam:
 			recontigSam(args[1], ejectedfn, mapping, clstr);
-			return;
+			break;
 		default:
 			stderr.writeln("Error: Filetype not supported.");
-			return;
+			return 1;
 	}
+	return 0;
 }
